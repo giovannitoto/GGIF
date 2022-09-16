3b207d1e3e61747038ee9c4d2ece9b5fcca60b9c
giovannitoto <52751929+giovannitoto@users.noreply.github.com>
2022-09-16 14:58:42 +0200

    data

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' @title W
+#' @description A nxc matrix of covariates.
+#' @format A data frame with 30 rows and 11 columns.
+#' @details DETAILS
+"W"
+
+#' @title X
+#' @description A pxq matrix of meta-covariates.
+#' @format A matrix with 500 rows and 5 columns.
+#' @details DETAILS
+"X"
+
+#' @title Y
+#' @description A nxp matrix of counts.
+#' @format A matrix with 30 rows and 500 columns.
+#' @details DETAILS
+"Y"
